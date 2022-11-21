2a26822afc7f8f4a080ab5ef5d897156ace84987
AiorNerok <prerson12ru@gmail.com>
2022-11-21 22:41:58 +0300

    Create Exception

--- bot/app.py
+++ bot/app.py
@@ -1,35 +1,63 @@
-import redis
-import json
 import telebot
+from telebot import types
 from dotenv import load_dotenv, dotenv_values
 
-from extensions import *
+from extensions import API, number_separator, BadRequestError
 
 load_dotenv()
 config = dict(dotenv_values(".env"))
 
-
-redis_connect = redis.Redis(
-    host=config["REDIS_HOST"],
-    port=config["REDIS_PORT"],
-    password=config["REDIS_PASSWORD"],
-)
-
-
-# red.set("currency", str(currency))
-currency = redis_connect.get("currency").decode("utf8").replace("'", '"')
-currency = json.loads(currency)
-
-currency_values = currency.values()
-
+currency = {"Российский рубль": "RUR", "Доллар США": "USD", "Евро": "EUR"}
 
 bot = telebot.TeleBot(config["TOKEN"])
 
 
-@bot.message_handler(commands=["/info"], content_types=["text"])
-def info_currency(msg: telebot.types.Message):
-    print(msg)
-    bot.send_message(msg.chat.id, msg)
+@bot.message_handler(commands=["values"])
+def values(msg: types.Message):
+    bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""<pre>
+Доступные валюты:
+________________________
+|Наименование    | код |
+|----------------|-----|
+|Российский рубль| RUR |
+|Доллар США      | USD |
+|Евро            | EUR |
+------------------------
+</pre>""",
+        parse_mode="HTML",
+    )
+
+
+@bot.message_handler(commands=["help"])
+def help(msg: types.Message):
+    bot.send_message(msg.chat.id, '''Пример запроса "RUR USD 250"''')
+
+
+@bot.message_handler(content_types=["text"])
+def convert_currency(msg: types.Message):
+
+    
+    try:
+        base, quote, amount = msg.text.split()
+
+        if all([x in list(currency.values()) for x in [base, quote]]):
+
+            result = API.get_price(base, quote, amount)
+
+            bot.send_message(
+                msg.chat.id,
+                f"""
+    {base} - {number_separator(amount)[:-2]}
+    {quote} - {number_separator(round(result["data"]["sum_result"], 2))}
+                """,
+            )
+        else:
+            pass
+        
+    except BadRequestError:
+        print(BadRequestError)
 
 
 bot.polling(non_stop=True)
--- bot/extensions.py
+++ bot/extensions.py
@@ -1,15 +1,58 @@
 import requests
 import json
-import lxml
-import lxml.html
 
 
-class CurrencyConvertApi:
-    _main_url = "https://cash.rbc.ru/cash/json/converter_currency_rate"
+def validate_arguments(func):
+    def wrapper(*args, **kwargs):
+        if len(set(args)) != 3:
+            raise BadRequestError(
+                """
+                    Number of arguments should be 3
+                    Example "RUR USD 100"
+                                    """
+            )
 
-    def convert(self, currency_from, currency_to, amount):
+        if args[0] == args[1]:
+            raise BadRequestError(
+                """
+                    The first argument and the second argument are equal
+                    But must be different
+                                    """
+            )
+
+        if not args[2].isdigit():
+            raise BadRequestError(
+                """
+                    the last argument must be an integer greater than 0
+                                    """
+            )
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+class API:
+    @validate_arguments
+    @staticmethod
+    def get_price(base: str, quote: str, amount: str):
+        _main_url = "https://cash.rbc.ru/cash/json/converter_currency_rate"
         req = requests.get(
-            f"{self._main_url}/?currency_from={currency_from}&currency_to={currency_to}&source=cbrf&sum={amount}&date="
+            f"{_main_url}/?currency_from={base}&currency_to={quote}&source=cbrf&sum={amount}&date="
         )
-        value = json.loads(req.content)
-        return value
+
+        result = json.loads(req.content)
+        return result
+
+
+class BadRequestError(Exception):
+    pass
+
+
+
+def number_separator(num):
+    return "{0:,}".format(round(float(num), 2)).replace(",", " ")
+
+
+if __name__ == "__main__":
+    pass
